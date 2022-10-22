063ad7ba06aad6711c4f0bd471eac8f681178231
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-10-23 02:21:33 +0600

    add register info varification

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -4,16 +4,22 @@ import Form from 'react-bootstrap/Form';
 const Register = () => {
     const handleSubmite = event => {
         event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photo = form.photoURL.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, photo, email, password);
     }
     return (
         <Form onSubmit={handleSubmite}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Name</Form.Label>
-                <Form.Control type="name" name='text' placeholder="Enter name" />
+                <Form.Control type="text" name='name' placeholder="Enter name" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>photoURL</Form.Label>
-                <Form.Control type="photoURL" name='text' placeholder="Enter photoURL" />
+                <Form.Control type="text" name='photoURL' placeholder="Enter photoURL" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
@@ -22,7 +28,7 @@ const Register = () => {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="text" name='password' placeholder="Password" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Form.Check type="checkbox" label="Check me out" />
